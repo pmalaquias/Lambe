12ce19563572de1d61563b178a494bf2f18a61d5
pmalaquias <pedrode13@gmail.com>
2020-07-23 11:57:28 -0300

    Componente Cabeçalho

---
+++ src/components/Header.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import {
+    StyleSheet,
+    Text,
+    View,
+    Platform,
+    Image
+} from 'react-native'
+import icon from '../../assets/imgs/icon.png'
+
+
+class Header extends Component {
+    render(){
+        return(
+            <View style={styles.container}>
+                <View style={styles.rowContainer}>
+                    <Image source={icon} style={styles.image}/>
+                    <Text style={styles.tilte}>Lambe Lambe</Text>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        marginTop: Platform.os === 'ios' ? 20 : 15,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: '#BBB'
+    },
+    rowContainer: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    image :{
+        height: 30,
+        width: 30,
+        resizeMode: 'contain'
+    },
+    tilte: {
+        color: '#000',
+        height: 30,
+        fontSize: 28
+    }
+})
+
+export default Header
