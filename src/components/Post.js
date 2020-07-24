020c0a8eb9f95c015cbcb3bdb5f021ca8e418168
pmalaquias <pedrode13@gmail.com>
2020-07-24 11:16:07 -0300

    Adicionar comentarios

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,6 +3,7 @@ import { StyleSheet, View, Image, Dimensions } from 'react-native'
 
 import Author from './Author'
 import Comments from './Comments'
+import AddComment from './AddComment'
 
 class Post extends Component{
     render(){
@@ -11,7 +12,7 @@ class Post extends Component{
                 <Image source={this.props.image} style={styles.image} />
                 <Author email='fulano@gmail.com' nickname='Fulano de Tal'/>
                 <Comments comments={this.props.comments} />
-                
+                <AddComment />
             </View>
         )
     }
